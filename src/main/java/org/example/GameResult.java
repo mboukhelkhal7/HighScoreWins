86da52bf2ab2a04305fb19b13046fc4faae21bf7
Mahmoud <maboukhelkhal@gmail.com>
2025-04-17 01:41:33 -0700

    initial commit

---
+++ src/main/java/org/example/GameResult.java
@@ -0,0 +1,48 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class GameResult {
+
+    static Scanner input = new Scanner(System.in);
+
+    public String teamA;
+    public String teamB;
+    public int scoreA;
+    public int scoreB;
+    public String teams;
+    public String scores;
+
+
+    // methods
+
+    public String getScore(){
+        System.out.println("please Enter the game score");
+        return input.nextLine();       // returning user input
+    }
+
+    public void splitScore(String score) {
+        teams = score.split("\\|")[0];
+        scores = score.split("\\|")[1];
+
+    }
+
+    public void splitTeams() {
+        teamA = teams.split(":")[0];
+        teamB = teams.split(":")[1];
+        scoreA = Integer.parseInt(scores.split(":")[0]);
+        scoreB = Integer.parseInt(scores.split(":")[1]);
+    }
+
+    public void getWinner() {
+        if (scoreA > scoreB){
+            System.out.println("the winners is" + teamA);
+        } else if (scoreB > scoreA){
+            System.out.printf("this winner is %s", teamB);
+        }else{
+            System.out.println("the game is a draw");
+        }
+
+
+    }
+}
